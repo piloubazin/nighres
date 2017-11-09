e9bdc28aba47182afb5c1177e71a5443df88978c
piloubazin <piloubazin@gmail.com>
2017-11-09 13:35:21 +0100

    Update __init__.py

--- nighres/__init__.py
+++ nighres/__init__.py
@@ -5,7 +5,7 @@ import data
 import laminar
 import surface
 import segmentation
-import filter
+import filtering
 from global_settings import ATLAS_DIR, TOPOLOGY_LUT_DIR, DEFAULT_ATLAS
 
 __all__ = ['io', 'brain', 'laminar', 'surface', 'data', 'segmentation', 'filter', '__version__']
