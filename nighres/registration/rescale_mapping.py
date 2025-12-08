0b00f05654d7610016390d85058024b59739e871
Pilou Bazin <piloubazin@gmail.com>
2025-12-08 16:24:04 +0100

    add 2D rescaling

--- nighres/registration/rescale_mapping.py
+++ nighres/registration/rescale_mapping.py
@@ -131,3 +131,108 @@ def rescale_mapping(source_image=None,
         outputs = {'mapping': mapping_img, 'inverse': inverse_img}
 
     return outputs
+
+def rescale_mapping_2d(source_image=None,
+            scaling_factor=1.0,
+            save_data=False, overwrite=False, output_dir=None,
+            file_name=None):
+    """ Rescale
+
+    Generate a pair of coordinate mappings for image rescaling in 2D
+
+    Parameters
+    ----------
+    source_image: niimg
+        Image to rescale
+    scaling_factor: float
+        Factor to use for rescaling
+    save_data: bool
+        Save output data to file (default is False)
+    overwrite: bool
+        Overwrite existing results (default is False)
+    output_dir: str, optional
+        Path to desired output directory, will be created if it doesn't exist
+    file_name: str, optional
+        Desired base name for output files with file extension
+        (suffixes will be added)
+
+    Returns
+    ----------
+    dict
+        Dictionary collecting outputs under the following keys
+        (suffix of output files in brackets)
+
+        * mapping (niimg): Coordinate mapping from source to rescaled (_sc-map)
+        * inverse (niimg): Inverse coordinate mapping from rescaled to source (_sc-invmap)
+
+    Notes
+    ----------
+    """
+
+    print('\nRescale mapping 2D')
+
+    # make sure that saving related parameters are correct
+    if save_data:
+        output_dir = _output_dir_4saving(output_dir, source_image) # needed for intermediate results
+        mapping_file = os.path.join(output_dir, 
+                        _fname_4saving(module=__name__,file_name=file_name,
+                                   rootfile=source_image,
+                                   suffix='sc-map'))
+
+        inverse_file = os.path.join(output_dir, 
+                        _fname_4saving(module=__name__,file_name=file_name,
+                                   rootfile=source_image,
+                                   suffix='sc-invmap'))
+
+        if overwrite is False \
+           and os.path.isfile(mapping_file) \
+            and os.path.isfile(inverse_file) :
+            
+            print("skip computation (use existing results)")
+            output = {'mapping': mapping_file, 'inverse': inverse_file}
+            return output
+
+
+    # load and get dimensions and resolution from input images
+    source = load_volume(source_image)
+    affine = source.affine
+    srcheader =  source.header
+    trgheader = source.header.copy()
+    nx = source.header.get_data_shape()[X]
+    ny = source.header.get_data_shape()[Y]
+    rx = source.header.get_zooms()[X]
+    ry = source.header.get_zooms()[Y]
+    
+    nsx = math.ceil(nx*scaling_factor)
+    nsy = math.ceil(ny*scaling_factor)
+    rsx = rx/scaling_factor
+    rsy = ry/scaling_factor
+    if len(source.header.get_zooms())==2:
+        trgheader.set_zooms((rsx,rsy))
+    elif len(source.header.get_zooms())==3:
+        trgheader.set_zooms((rsx,rsy,source.header.get_zooms()[3]))
+
+    # build coordinate mapping matrices and save them to disk
+    mapping = numpy.zeros((nsx,nsy,2))
+    for x in range(nsx):
+        for y in range(nsy):
+                mapping[x,y,X] = x/scaling_factor
+                mapping[x,y,Y] = y/scaling_factor
+                
+    inverse = numpy.zeros((nx,ny,2))
+    for x in range(nx):
+        for y in range(ny):
+            inverse[x,y,X] = x*scaling_factor
+            inverse[x,y,Y] = y*scaling_factor
+
+    mapping_img = nibabel.Nifti1Image(mapping, affine, trgheader)
+    inverse_img = nibabel.Nifti1Image(inverse, affine, srcheader)
+
+    if save_data:
+        save_volume(mapping_file, mapping_img)
+        save_volume(inverse_file, inverse_img)
+        outputs = {'mapping': mapping_file, 'inverse': inverse_file}
+    else:
+        outputs = {'mapping': mapping_img, 'inverse': inverse_img}
+
+    return outputs
