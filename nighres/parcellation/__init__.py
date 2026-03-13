a184e2237d38b3c58f6d12a5c4f1f8b61eacb036
Pilou Bazin <piloubazin@gmail.com>
2026-03-13 08:55:57 +0100

    cleaned-up version of the MASSP approach with LC support

--- nighres/parcellation/__init__.py
+++ nighres/parcellation/__init__.py
@@ -1,11 +1,11 @@
 from nighres.parcellation.massp import massp
 from nighres.parcellation.massp import massp_atlasing
-from nighres.parcellation.massp import massp2p1
-from nighres.parcellation.massp import massp2p1_atlasing
 from nighres.parcellation.massp import massp_17structures_label
 from nighres.parcellation.massp import massp_17structures_list
 from nighres.parcellation.massp import massp_2p0_label
 from nighres.parcellation.massp import massp_2p0_list
+from nighres.parcellation.massp import massp_2p1_label
+from nighres.parcellation.massp import massp_2p1_list
 from nighres.parcellation.super_voxels import super_voxels
 from nighres.parcellation.parcellation_smoothing import parcellation_smoothing
 
